beb79eb35b3ad4fd66b949006942f508d76ea7c9
sgelb <stephan.halbritter@haw-hamburg.de>
2016-01-27 11:55:58 +0100

    Small changes in fontsize.sh

--- fontsize.sh
+++ fontsize.sh
@@ -3,20 +3,20 @@
 # Change console fontsize 
 
 # exit if any statement returns non-true
-# if we don't care, use: command || true 
 set -o errexit
 
 # exit script if using unset variable
 set -o nounset
 
-default=`grep "^[^#]*font: " ~/.Xdefaults | sed 's/^.*font: //g'`
+default=$(grep "^[^#]*font: " ~/.Xresources | sed 's/^.*font: //g')
 
 if [ $# -ne 1 ]; then
-    echo "Reset to .Xdefaults"
-    printf '\33]50;%s%d\007' "${default}"
+  echo "Setting font size to value set in .Xresources"
+  printf '\33]50;%s%d\007' "${default}"
 elif [[ $1 =~ ^[0-9]*$ ]]; then
-    new=`echo ${default} | sed -e s/size=[0-9]*/size=$1/g`
-    printf '\33]50;%s%d\007' "${new}"
+  echo "Setting font size to ${1}"
+  new=$(echo "${default}" | sed -e "s/\(size=\)[0-9]*/\1$1/")
+  printf '\33]50;%s%d\007' "${new}"
 else
-    echo "Invalid argument"
+  echo "Invalid argument"
 fi
